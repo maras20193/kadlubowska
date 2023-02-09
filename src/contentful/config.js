d7871ca7281771d1e35b8bd34b1956264e06736c
Łukasz Markowski <lucas20193@gmail.com>
2023-02-09 18:47:00 +0100

    feat(app): update env and site-map

--- src/contentful/config.js
+++ src/contentful/config.js
@@ -1,6 +1,6 @@
 import { createClient } from 'contentful';
 
 export const client = createClient({
-  space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_KEY,
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY,
 });
