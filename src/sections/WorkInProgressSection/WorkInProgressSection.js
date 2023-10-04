6ec51ac0b1413e99945cfaa06c20ff92ebe0b4c8
Łukasz Markowski <lucas20193@gmail.com>
2023-10-04 17:53:11 +0200

    [F] Add fonts and metatags

---
+++ src/sections/WorkInProgressSection/WorkInProgressSection.js
@@ -0,0 +1,5 @@
+import * as S from './WorkInProgressSection.styled';
+
+export const WorkInProgressSection = () => (
+  <S.Wrapper>Work in progress</S.Wrapper>
+);
--- src/sections/index.js
+++ src/sections/index.js
@@ -4,3 +4,4 @@ export { ServicesSection } from './ServicesSection/ServicesSection';
 export { GallerySection } from './GallerySection/GallerySection';
 export { ReviewsSection } from './ReviewsSection/ReviewsSection';
 export { FooterSection } from './FooterSection/FooterSection';
+export { WorkInProgressSection } from './WorkInProgressSection/WorkInProgressSection';
